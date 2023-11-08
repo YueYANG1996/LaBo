8a7394f08b1abf96ece6decb0cb58ed94dbda0f3
Jason Uwaeze <34732790+Wazhee@users.noreply.github.com>
2023-11-08 09:33:56 -0600

    Update labo_train.sh
    In labo_train.sh you defined "cfg/asso_opt/$2/$2_$1shot_fac.py" as the config filepath.
    
    If $1 is {Dataset} & $2 is {shots} as defined in the readme, the labo_train.sh should be defined as the following:
    
    cfg/asso_opt/$1/$1_$2shot_fac.py

--- labo_train.sh
+++ labo_train.sh
@@ -1,4 +1,4 @@
 # $1: number of shots
 # $2: dataset (flower/food101)
 
-python main.py --cfg cfg/asso_opt/$2/$2_$1shot_fac.py --work-dir exp/asso_opt/$2/$2_$1shot_fac --func $asso_opt_main ${@:3}
+python main.py --cfg cfg/asso_opt/$1/$1_$2shot_fac.py --work-dir exp/asso_opt/$2/$2_$1shot_fac --func $asso_opt_main ${@:3}
